60c60784c2e964db99f5e63ad460d03f2b12f556
Dipak kokate <dk6939980@gmail.com>
2026-07-14 09:15:33 +0530

    Add Jenkins pipeline for webserver deployment

--- jenkins/deckarative.groovy
+++ jenkins/deckarative.groovy
@@ -1,1 +1,31 @@
 
+pipeline {
+    agent { label 'webserver' }
+    stages {
+        stage('pull') {
+            steps {
+                echo "we are pulling from webserver"
+                git 'https://github.com/dipakkokate/cdec-b3.git'
+            }
+        }
+
+        stage('Build') {
+            steps {
+                echo "building this app usig maven"
+                
+            }
+        }
+        stage('Test') {
+            steps {
+                echo "testing this app using sonarqube"
+                
+            }
+        }
+        stage('Deploy') {
+            steps {
+                echo "deploying this app using webserver"
+                
+            }
+        }
+    }
+}
